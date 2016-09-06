5d2291d041a16f551318f86d402b621fee7bde48
Cooper Knaak <cooperknaak@gmail.com>
2016-09-06 13:36:56 -0500

    Initial Commit

---
+++ CoronaStructures/CoronaStructures.h
@@ -0,0 +1,19 @@
+//
+//  CoronaStructures.h
+//  CoronaStructures
+//
+//  Created by Cooper Knaak on 9/6/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoronaStructures.
+FOUNDATION_EXPORT double CoronaStructuresVersionNumber;
+
+//! Project version string for CoronaStructures.
+FOUNDATION_EXPORT const unsigned char CoronaStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoronaStructures/PublicHeader.h>
+
+
